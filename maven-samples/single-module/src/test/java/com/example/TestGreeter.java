2eeb73ecbfa340ecaea42e7a402b1105132371ea
Kotesh, Munugoti <munugotik@yahoo.com>
2020-04-14 17:56:11 +0100

    updated tests

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -31,6 +31,13 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  @Test
+  public void greetShouldIncludeGreetingPhrase1() {
+    String someone = "World - Kotesh";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
